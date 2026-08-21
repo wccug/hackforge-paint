8c7cd77a1142b78bcb7849add52a053d35bcdbc9
Bryan St. Amour <bryan@bryanstamour.com>
2026-08-20 20:29:57 -0400

    Linux colorpicker
    Make it more exciting by selecting a random colour for you! We'll
    eventually fill this in, but it's kind of cool for the time being.

--- src/linux/colorpicker.cpp
+++ src/linux/colorpicker.cpp
@@ -1,9 +1,16 @@
 #include "../colorpicker.hpp"
 
+#include <random>
+
 namespace hackforge {
 
 SDL_Color OpenNativeColorPicker(SDL_Window *window, SDL_Color currentColor) {
-  return {.r = 0, .g = 255, .b = 0, .a = 255};
+  static std::random_device rd{};
+  static std::mt19937 engine(rd());
+  std::uniform_int_distribution<std::uint8_t> dist{0, 255};
+  auto sample = [&]() { return dist(engine); };
+
+  return {.r = sample(), .g = sample(), .b = sample(), .a = 255};
 }
 
 } // namespace hackforge
